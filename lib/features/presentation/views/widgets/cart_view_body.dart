81b2972ad6b153fbd68fa1720ffedbeeff5c2778
Mohamed <mohamadnafe14@gmail.com>
2026-09-21 15:20:27 +0300

    finishing cart view UI

--- lib/features/presentation/views/widgets/cart_view_body.dart
+++ lib/features/presentation/views/widgets/cart_view_body.dart
@@ -8,26 +8,24 @@ class CartViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
-          children: [
-            SizedBox(height: 20),
-            Image.asset('assets/images/basket_image.png'),
-            SizedBox(height: 20),
-            OrderInfo(orderKey: "Order Subtotal", value: "\$42.00"),
-            const SizedBox(height: 3),
-            OrderInfo(orderKey: "Discount", value: "\$0"),
-            const SizedBox(height: 3),
-            OrderInfo(orderKey: "Shipping", value: "\$8"),
-            const Divider(thickness: 2, height: 34),
-            TotalPrice(title: "Total", value: "\$50.00"),
-            const SizedBox(height: 16),
-            CustomButton(onPressed: () => {}),
-            const SizedBox(height: 16),
-          ],
-        ),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children: [
+          SizedBox(height: 20),
+          Expanded(child: Image.asset('assets/images/basket_image.png')),
+          SizedBox(height: 20),
+          OrderInfo(orderKey: "Order Subtotal", value: "\$42.00"),
+          const SizedBox(height: 3),
+          OrderInfo(orderKey: "Discount", value: "\$0"),
+          const SizedBox(height: 3),
+          OrderInfo(orderKey: "Shipping", value: "\$8"),
+          const Divider(thickness: 2, height: 34),
+          TotalPrice(title: "Total", value: "\$50.00"),
+          const SizedBox(height: 16),
+          CustomButton(onPressed: () => {}),
+          const SizedBox(height: 16),
+        ],
       ),
     );
   }
