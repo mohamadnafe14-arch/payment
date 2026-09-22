1f16f04ba138b596a5b23f574113b77795dc84e6
Mohamed <mohamadnafe14@gmail.com>
2026-09-23 01:56:24 +0300

    adding changing active status to payment item

--- lib/features/presentation/views/widgets/payment_details_body.dart
+++ lib/features/presentation/views/widgets/payment_details_body.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:payment/features/presentation/views/widgets/payment_item.dart';
 import 'package:payment/features/presentation/views/widgets/payment_list.dart';
 
 class PaymentDetailsBody extends StatelessWidget {
--- lib/features/presentation/views/widgets/payment_list.dart
+++ lib/features/presentation/views/widgets/payment_list.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:payment/features/presentation/views/widgets/payment_item.dart';
 
-class PayementList extends StatelessWidget {
-  PayementList({super.key});
+class PayementList extends StatefulWidget {
+  const PayementList({super.key});
+
+  @override
+  State<PayementList> createState() => _PayementListState();
+}
+
+class _PayementListState extends State<PayementList> {
   final List<String> images = <String>[
     'assets/images/card.svg',
     'assets/images/paypal.svg',
     'assets/images/master_card.svg',
   ];
+  int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -18,7 +25,19 @@ class PayementList extends StatelessWidget {
         itemBuilder: (context, index) {
           return Padding(
             padding: const EdgeInsets.symmetric(horizontal: 8),
-            child: PaymentItem(image: images[index]),
+            child: GestureDetector(
+              onTap: () {
+                if (currentIndex != index) {
+                  setState(() {
+                    currentIndex = index;
+                  });
+                }
+              },
+              child: PaymentItem(
+                image: images[index],
+                isSlected: index == currentIndex,
+              ),
+            ),
           );
         },
       ),
