6cf07b103f8707a3b0bf3fa19e405fb1a90b1532
Mohamed <mohamadnafe14@gmail.com>
2026-09-23 12:09:46 +0300

    adding payment credit card form

--- lib/features/presentation/views/widgets/custom_creadit_card.dart
+++ lib/features/presentation/views/widgets/custom_creadit_card.dart
@@ -14,16 +14,37 @@ class _CustomCreaditCardState extends State<CustomCreaditCard> {
   String cardHolderName = '';
   String cvvCode = '';
   bool showBackView = false;
-
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return CreditCardWidget(
-      cardNumber: cardNumber,
-      expiryDate: expiryDate,
-      cardHolderName: cardHolderName,
-      cvvCode: cvvCode,
-      showBackView: showBackView,
-      onCreditCardWidgetChange: (onChange) {},
+    return Column(
+      children: [
+        CreditCardWidget(
+          cardNumber: cardNumber,
+          expiryDate: expiryDate,
+          cardHolderName: cardHolderName,
+          cvvCode: cvvCode,
+          showBackView: showBackView,
+          isHolderNameVisible: true,
+          onCreditCardWidgetChange: (onChange) {},
+        ),
+        CreditCardForm(
+          formKey: formKey,
+          cardNumber: cardNumber,
+          expiryDate: expiryDate,
+          cardHolderName: cardHolderName,
+          cvvCode: cvvCode,
+          onCreditCardModelChange: (cardDetails) {
+            setState(() {
+              cardNumber = cardDetails.cardNumber;
+              expiryDate = cardDetails.expiryDate;
+              cardHolderName = cardDetails.cardHolderName;
+              cvvCode = cardDetails.cvvCode;
+              showBackView = cardDetails.isCvvFocused;
+            });
+          },
+        ),
+      ],
     );
   }
 }
--- lib/features/presentation/views/widgets/payment_details_body.dart
+++ lib/features/presentation/views/widgets/payment_details_body.dart
@@ -7,6 +7,8 @@ class PaymentDetailsBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [PayementList(), CustomCreaditCard()]);
+    return SingleChildScrollView(
+      child: Column(children: [PayementList(), CustomCreaditCard()]),
+    );
   }
 }
