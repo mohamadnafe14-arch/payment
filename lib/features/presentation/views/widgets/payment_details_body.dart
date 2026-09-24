98afbfc4a45b090c1fb368915cffcf65d3a6f808
Mohamed <mohamadnafe14@gmail.com>
2026-09-24 10:24:27 +0300

    adding button to payment details body

--- lib/features/presentation/views/widgets/payment_details_body.dart
+++ lib/features/presentation/views/widgets/payment_details_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:payment/features/presentation/views/widgets/custom_button.dart';
 import 'package:payment/features/presentation/views/widgets/custom_creadit_card.dart';
 import 'package:payment/features/presentation/views/widgets/payment_list.dart';
 
@@ -7,8 +8,21 @@ class PaymentDetailsBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(children: [PayementList(), CustomCreaditCard()]),
+    return CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(child: PayementList()),
+        SliverToBoxAdapter(child: CustomCreaditCard()),
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: CustomButton(onPressed: () {}),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
