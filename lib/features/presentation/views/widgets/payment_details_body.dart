dde18b2163a16095646920e3ea0e089126fae58c
Mohamed <mohamadnafe14@gmail.com>
2026-09-24 11:10:11 +0300

    adding thank you page

--- lib/features/presentation/views/widgets/payment_details_body.dart
+++ lib/features/presentation/views/widgets/payment_details_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:payment/features/presentation/views/thank_you_view.dart';
 import 'package:payment/features/presentation/views/widgets/custom_button.dart';
 import 'package:payment/features/presentation/views/widgets/custom_creadit_card.dart';
 import 'package:payment/features/presentation/views/widgets/payment_list.dart';
@@ -12,31 +13,41 @@ class PaymentDetailsBody extends StatefulWidget {
 
 class _PaymentDetailsBodyState extends State<PaymentDetailsBody> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-   AutovalidateMode _autovalidateMode = AutovalidateMode.disabled;
+  AutovalidateMode _autovalidateMode = AutovalidateMode.disabled;
   @override
   Widget build(BuildContext context) {
     return CustomScrollView(
       slivers: [
         SliverToBoxAdapter(child: PayementList()),
-        SliverToBoxAdapter(child: CustomCreaditCard(
-          formKey:_formKey,
-          autovalidateMode:_autovalidateMode
-        )),
+        SliverToBoxAdapter(
+          child: CustomCreaditCard(
+            formKey: _formKey,
+            autovalidateMode: _autovalidateMode,
+          ),
+        ),
         SliverFillRemaining(
           hasScrollBody: false,
           child: Align(
             alignment: Alignment.bottomCenter,
             child: Padding(
               padding: const EdgeInsets.all(8.0),
-              child: CustomButton(onPressed: () {
-                if(_formKey.currentState!.validate()){
-                  _formKey.currentState!.save();
-                } else {
-                  setState(() {
-                    _autovalidateMode = AutovalidateMode.always;
-                  });
-                }
-              }),
+              child: CustomButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _formKey.currentState!.save();
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const ThankYouView(),
+                      ),
+                    );
+                  } else {
+                    setState(() {
+                      _autovalidateMode = AutovalidateMode.always;
+                    });
+                  }
+                },
+              ),
             ),
           ),
         ),
