d62eb22178c03501c4fe4f6923b372b13ab1d5f7
Mohamed <mohamadnafe14@gmail.com>
2026-09-25 15:25:07 +0300

    adding check icon

--- lib/features/presentation/views/widgets/thank_you_body.dart
+++ lib/features/presentation/views/widgets/thank_you_body.dart
@@ -8,10 +8,11 @@ class ThankYouBody extends StatelessWidget {
     return Padding(
       padding: EdgeInsets.all(32.0),
       child: Stack(
+        clipBehavior: Clip.none,
         children: [
           Container(
             decoration: BoxDecoration(
-              color: Colors.grey,
+              color: Colors.grey.withValues(alpha: 0.2),
               shape: BoxShape.rectangle,
               borderRadius: BorderRadius.circular(10),
             ),
@@ -25,6 +26,20 @@ class ThankYouBody extends StatelessWidget {
             bottom: MediaQuery.of(context).size.height * 0.2,
             left: -20,
             child: CircleAvatar(backgroundColor: Colors.white),
+          ),
+          Positioned(
+            top: -40,
+            right: 0,
+            left: 0,
+            child: CircleAvatar(
+              backgroundColor: Colors.grey.withValues(alpha: 0.2),
+              radius: 50,
+              child: CircleAvatar(
+                backgroundColor: Colors.green,
+                radius: 40,
+                child: Icon(Icons.check, size: 40, color: Colors.white),
+              ),
+            ),
           ),
         ],
       ),
